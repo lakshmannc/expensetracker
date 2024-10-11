272103d5c5a410066de50f993c3267564f5d0e3a
Raamgopal8 <raamgopal2369@outlook.com>
2024-10-11 14:56:16 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
+import LoginSignup from './Pages/LoginSignup';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+
 
 function App() {
   return (
     <div>
-     <Navbar/>      
+    <BrowserRouter>  
+      <Navbar/>
+      <Routes> 
+      
+      
+      <Route path = '/login' element = {<LoginSignup/>}/>
+      
+      </Routes>
+    </BrowserRouter>
     </div>
   );
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
 import './Navbar.css'
+import{ Link } from 'react-router-dom'
 
 const Navbar = () => {
     return (
         <div className='navbar'>
             <div className='logo'>
                 <img src = "" alt = ""/>
-                <p>EXPENSE TRACKERS</p>
+                <p>EXPENSE TRACKER</p>
             </div>
             <ul className="nav-menu">
                 <li>BUDGET ASSISTANT</li>
                 
             </ul>
             <div className='nav-login'>
-                <button>login</button>
+            <Link to = '/login'><button>Login</button></Link>
 
                 
             </div>
