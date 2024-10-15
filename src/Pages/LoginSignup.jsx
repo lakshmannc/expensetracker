aedf39dbecf257e119f22a5b2d1db0d560d19c7d
Raamgopal8 <raamgopal2369@outlook.com>
2024-10-15 19:51:17 +0530

    new

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -3,25 +3,19 @@ import '../Pages/CSS/LoginSignup.css';
 
 const LoginSignup = () => {
     return (
-        <div className='loginsignup'>
-            <div className="loginsignup-container">
-                <h1>Sign up</h1>
-                <div className="loginsignup-fields">
-                    <input type = "text" placeholder='Your Name'/>
-                    <input type = "email" placeholder=' Email'/>
-                    <input type = "password" placeholder='Password'/>
-
+        <div className='container'>
+            <div className="header">
+                <div className="text">Sign up</div>
+                <div className="underline"></div>    
+            </div>
+            <div className="inputs">
+                <div className="input">
+                    
                 </div>
-                <button>Continue</button>
-                <p className="loginsignup-login">Already have an account?<span>Login Here</span></p>
-            <div className="loginsignup-agree">
-                <input type="checkbox" name ='' id = '' />
-                <p>By Continuing ,I agree to terms and conditions.</p>
             </div>
             </div>
 
-        </div>
+        
     )
 }
-
 export default LoginSignup
