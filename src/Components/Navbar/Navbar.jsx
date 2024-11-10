397b0089cc38b3126d49c437e097e9774b3e685a
Raamgopal8 <raamgopal2369@outlook.com>
2024-11-10 23:03:30 +0530

    SideBar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,21 +1,96 @@
-import React from 'react'
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+import ListIcon from '@mui/icons-material/List';
+import NotificationsIcon from '@mui/icons-material/Notifications';
 import './Navbar.css'
 import{ Link } from 'react-router-dom'
-import logo from '../assests/bank.jpg'
+
 const Navbar = () => {
+   
+    const [state, setState] = React.useState({
+        top: false,
+        left: false,
+        bottom: false,
+        right: false,
+      });
+
+      const toggleDrawer = (anchor, open) => (event) => {
+        if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+          return;
+        }
+    
+        setState({ ...state, [anchor]: open });
+      };
+      const list = (anchor) => (
+        <Box
+          sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
+          role="presentation"
+          onClick={toggleDrawer(anchor, false)}
+          onKeyDown={toggleDrawer(anchor, false)}
+        >
+          <List>
+            {['Home', 'Dashboard', 'Tracking','Income'].map((text, index) => (
+              <ListItem key={text} disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                  </ListItemIcon>
+                  <ListItemText primary={text} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+          <Divider />
+          <List>
+            {['Settings', 'User'].map((text, index) => (
+              <ListItem key={text} disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                  </ListItemIcon>
+                  <ListItemText primary={text} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+      );
+
     return (
-        <div className='navbar'>
-            <div className='logo'>
-                <img src = {logo} alt = ""/>
-            </div>
-            
-            <ul className="nav-menu">
-                <li>BUDGET ASSISTANT</li>
-            </ul>
-            <div className='nav-login'>
-            <Link to = '/login'><button>Login</button></Link>
+    <div className='navbar'>
+      
+      <div> 
+       <ListIcon 
+        onClick={toggleDrawer("left",true)
+        }
+        />
+        <Drawer
+            anchor={"left"}
+            open={state["left"]}
+            onClose={toggleDrawer("left", false)}
+          >
+            {list("left")}
+        </Drawer>
+      </div>
+
+      <div className="name">
+          <p>Budget Assistance</p>
+        </div> 
+         
+    <div className='nav-login'>
+        <NotificationsIcon/>
+    <Link to = '/login'> <button>Login</button></Link>
                 
-           </div>
+        </div>
         </div>
     )
 }
